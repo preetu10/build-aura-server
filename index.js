68f4bd7cf395821968e82b2d79420922b703d19f
Mahfuja <bajibaji1004@gmail.com>
2024-06-16 15:29:44 +0600

    1st commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const cors=require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port=process.env.PORT||5000;
+
+
+app.get('/',(req,res)=>{
+    res.send('hello world');
+});
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})
